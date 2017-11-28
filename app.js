58e54ea7ce023b1fa977004bfde542fc13cb87a1
Charles Ballew <charleswballewjr@gmail.com>
2017-11-28 11:36:17 -0700

    route and removed space and moved data to its own folder

--- app.js
+++ app.js
@@ -1,58 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const student = require("./data/student");
 app.use(cors());
 
-const students = [{
-    id: 1,
-    firstName: "Alice",
-    lastName: "Zephyr",
-    homeTown: "Seattle"
-},{
-    id: 2,
-    firstName: "Bob",
-    lastName: "Yellow",
-    homeTown: "Vancouver"
-},{
-    id: 3,
-    firstName: "Claire",
-    lastName: "Xylitol",
-    homeTown: "Toledo"
-},{
-    id: 4,
-    firstName: "Daniel",
-    lastName: "Winston",
-    homeTown: "Akron"
-},{
-    id: 5,
-    firstName: "Edina",
-    lastName: "Veritas",
-    homeTown: "Wichita"
-}];
-
 app.get("/", (req, res) => {
-    res.json({students});
+    res.json(student);
 });
 
-function itemId(objectItems, id) {
-    for (var i = 0; i < objectItems.length; i++) {
-        if(objectItems[i].id == id) {
-            return objectItems[i];
+app.get("/:id", (req, res) => {
+    for (var i = 0; i < student.students.length; i++) {
+        if (student.students[i].id == req.params.id) {
+            return res.json(student.students[i]);
         }
     }
-}
-
-app.get("/:id", (req, res) => {
-    var haveItem = itemId(students, req.params.id);
-    if(!haveItem) {
+    if(!student.students[i]) {
         res.status = 404;
         res.json({error: {message: "No record found!"}
         });
     }
-    res.json(haveItem);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("TS-3 Version-3 on localhost:3000");
 });
